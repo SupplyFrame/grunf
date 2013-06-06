730f0bf8ea8ee395b300eef18228d6434b21f699
Felix Chern <fchern@supplyframe.com>
2013-06-06 15:55:47 -0700

    Exception handling for file not found, config file verification etc.

--- src/grunf/bin.clj
+++ src/grunf/bin.clj
@@ -12,14 +12,24 @@
 
 
 (def log-pattern "%d{ISO8601}{GMT} [%-5p] [%t] - %m%n")
+(def cli-options
+  [["-c" "--config" "Path to the config file"]
+   ["-h" "--[no-]help" "Print this message" :default false]
+   ["--log" "log path for log4j. If not specified, log to console"]
+   ["--log-level" "log level for log4j, (fatal|error|warn|info|debug)" :default "debug"]])
+
+(defn- verify-config [config-array]
+  (assert (= (type config-array) clojure.lang.PersistentVector)
+          "Config should be an clojure array")
+  (doseq [config-array-element config-array]
+    (assert (= (type config-array-element) clojure.lang.PersistentArrayMap)
+            "Each element in config array should be a map")
+    (assert (:url config-array-element) "Must have :url in config map"))
+  config-array) ;; return the original config-array
 
 (defn -main [& argv]
   (let [[options args banner]
-        (cli argv
-             ["-c" "--config" "Path to the config file"]
-             ["-h" "--[no-]help" "Print this message" :default false]
-             ["--log" "log path for log4j. If not specified, log to console"]
-             ["--log-level" "log level for log4j, (fatal|error|warn|info|debug)" :default "debug"])]
+        (apply cli argv cli-options)]
     (when (or (:help options) (nil? argv))
       (println banner)
       (System/exit 0))
@@ -31,11 +41,18 @@
                                                      (:log options)
                                                      "'.'yyyy-MM-dd")
                           :console)})
-    (->> (try
-           (slurp (:config options))
-           (catch java.io.IOException e
-             (println "File not found:" (:config options))
-             (System/exit -1)))
-         (read-string) ;; should handle parse error exception
-         ;; should varify the parsed string
-         (pmap grunf/fetch))))
+    (pmap grunf/fetch
+          (try
+            (->> (:config options)
+                 (slurp)
+                 (read-string)
+                 (verify-config))
+            (catch java.io.IOException e
+              (println "Config file not found:" (:config options))
+              (System/exit -1))
+            (catch AssertionError e ;; clojure assertion
+              (println "config file error:" e)
+              (System/exit -1))
+            (catch Exception e
+              (println "Config file error" e)
+              (System/exit -1))))))
