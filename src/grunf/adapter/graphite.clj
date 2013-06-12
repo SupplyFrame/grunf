f7e652004ac03d0c6bc911c816d00016e1174438
Felix Chern <fchern@supplyframe.com>
2013-06-12 13:25:50 -0700

    Socket exception handling for graphite

--- src/grunf/adapter/graphite.clj
+++ src/grunf/adapter/graphite.clj
@@ -3,7 +3,7 @@
   (:require [clojure.string :as str])
   (:use clojure.template
         [grunf.core :only [GrunfOutputAdapter]])
-  (:import [java.net Socket]
+  (:import [java.net Socket ConnectException]
            [java.io PrintWriter]))
 
 (deftype Graphite [namespace host port]
@@ -11,30 +11,42 @@
   (log-success [this]
     (fn [{{start :start} :opts
           body :body}]
-      (binding [*out* (-> (Socket. (.host this) (.port this))
-                          (.getOutputStream)
-                          (PrintWriter.))]
-        (do-template [type value]
-                     (println (str (.namespace this) "." type)
-                              value
-                              (-> start (/ 1000) (int)))
-                     "response_time" (- (System/currentTimeMillis) start)
-                     "response_size" (count (map int body))
-                     "error" 0
-                     ))))
+      (try
+        (binding [*out* (-> (Socket. (.host this) (.port this))
+                            (.getOutputStream)
+                            (PrintWriter.))]
+          (do-template [type value]
+                       (println (str (.namespace this) "." type)
+                                value
+                                (-> start (/ 1000) (int)))
+                       "response_time" (- (System/currentTimeMillis) start)
+                       "response_size" (count (map int body))
+                       "error" 0
+                       ))
+        (catch ConnectException e
+          (println "Cannot connect to graphite server" (.host this) "port:" (.port this))
+          (println "try /opt/graphite/bin/carbon-cache.py start")))))
   (log-validate-error [this]
     (fn [{{start :start} :opts
           body :body}]
-      (binding [*out* (-> (Socket. (.host this) (.port this))
-                          (.getOutputStream)
-                          (PrintWriter.))]
-        (println (str (.namespace this) ".error") 1 (-> start (/ 1000) (int))))))
+      (try
+        (binding [*out* (-> (Socket. (.host this) (.port this))
+                            (.getOutputStream)
+                            (PrintWriter.))]
+          (println (str (.namespace this) ".error") 1 (-> start (/ 1000) (int))))
+        (catch ConnectException e
+          (println "Cannot connect to graphite server" (.host this) (.port this))          
+          (println "try /opt/graphite/bin/carbon-cache.py start")))))
   (log-redirect [this] (fn [_]))
   (log-client-error [this] (grunf.core/log-unknown-error this))
   (log-server-error [this] (grunf.core/log-unknown-error this))
   (log-unknown-error [this]
     (fn [{{start :start} :opts}]
-      (binding [*out* (-> (Socket. (.host this) (.port this))
-                          (.getOutputStream)
-                          (PrintWriter.))]
-        (println (str (.namespace this) ".error") 1 (-> start (/ 1000) (int)))))))
+      (try
+        (binding [*out* (-> (Socket. (.host this) (.port this))
+                            (.getOutputStream)
+                            (PrintWriter.))]
+          (println (str (.namespace this) ".error") 1 (-> start (/ 1000) (int))))
+        (catch ConnectException e
+          (println "Cannot connect to graphite server" (.host this) (.port this))
+          (println "try /opt/graphite/bin/carbon-cache.py start"))))))
