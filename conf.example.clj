9ad6d3dd8a832571ba00a8a06821eac7212dfa98
Felix Chern <fchern@supplyframe.com>
2013-07-02 09:57:40 -0700

    Update hack branch

--- conf.example.clj
+++ conf.example.clj
@@ -1,17 +1,14 @@
 [{
   :url "http://www.google.com"
-  :interval 50000
+  ;;:interval 50000
   ;;:validator #(re-find #"yahoo" %) ;; will show error :)
   :http-options {:timeout 2000
                  :user-agent "Mozilla"}
-  ;;:params-fn (map #(hash-map :id %) (iterate + 0))
+  :params-fn (map #(hash-map :id %) (iterate inc 0))
   ;; :graphite-ns "com.yahoo.www"
   ;; explicitly setup graphite namespace
 
   ;; sub namespace
   ;; graphite namespace would be "com.google.www.hoho"
-  }
- {
-  :url "http://google.com"
   }
 ]
