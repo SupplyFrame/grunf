df076c51bf33f99f61b6f67888fb3aadd3ef16e1
Felix Chern <fchern@supplyframe.com>
2013-08-07 10:27:22 -0700

    forgot to add riemann.clj

---
+++ src/grunf/adapter/riemann.clj
@@ -0,0 +1,53 @@
+(ns grunf.adapter.riemann
+  "riemann adapter"
+  (:use riemann.client
+        [grunf.core :only [GrunfOutputAdapter]]))
+
+(deftype RiemannAdapter [client]
+  GrunfOutputAdapter
+  (log-success [this]
+    (fn [{{start :start
+           url :url
+           ttl :interval} :opts}]
+      (send-event (.client this)
+                  {:service url
+                   :state "ok"
+                   :time (int (/ (System/currentTimeMillis) 1000))
+                   :tags ["grunf"]
+                   :description "query time"
+                   :metric (- (System/currentTimeMillis) start)
+                   :ttl (/ (* ttl 2) 1000)
+                   })))
+  (log-validate-error [this]
+    (fn [{{start :start
+           url :url
+           ttl :interval} :opts}]
+      (send-event (.client this)
+                  {:service url
+                   :state "warning"
+                   :time (int (/ (System/currentTimeMillis) 1000))
+                   :tags ["grunf"]
+                   :description "validation error"
+                   :metric (- (System/currentTimeMillis) start)
+                   :ttl (/ (* ttl 2) 1000)
+                   })))
+  (log-redirect [this] (fn [_]))
+  (log-client-error [this] (grunf.core/log-unknown-error this))
+  (log-server-error [this] (grunf.core/log-unknown-error this))
+  (log-unknown-error [this]
+    (fn [{{start :start
+           url :url
+           ttl :interval} :opts
+           error :error
+           status :status}]
+      (send-event (.client this)
+                  {:service url
+                   :state "error"
+                   :time (int (/ (System/currentTimeMillis) 1000))
+                   :tags ["grunf"]
+                   :description (str status ", error:" error)
+                   :metric (- (System/currentTimeMillis) start)
+                   :ttl (/ (* ttl 2) 1000)
+                   })))
+  )
+
