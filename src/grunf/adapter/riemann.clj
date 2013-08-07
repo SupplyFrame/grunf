f1b55096eae8b2273088104adc363bb280b063a6
Felix Chern <fchern@supplyframe.com>
2013-08-07 10:41:40 -0700

    catch tcp errors

--- src/grunf/adapter/riemann.clj
+++ src/grunf/adapter/riemann.clj
@@ -1,7 +1,8 @@
 (ns grunf.adapter.riemann
   "riemann adapter"
   (:use riemann.client
-        [grunf.core :only [GrunfOutputAdapter]]))
+        [grunf.core :only [GrunfOutputAdapter]])
+  (:import [java.io IOException]))
 
 (deftype RiemannAdapter [client]
   GrunfOutputAdapter
@@ -9,28 +10,31 @@
     (fn [{{start :start
            url :url
            ttl :interval} :opts}]
-      (send-event (.client this)
-                  {:service url
-                   :state "ok"
-                   :time (int (/ (System/currentTimeMillis) 1000))
-                   :tags ["grunf"]
-                   :description "query time"
-                   :metric (- (System/currentTimeMillis) start)
-                   :ttl (/ (* ttl 2) 1000)
-                   })))
+      (try
+        (send-event (.client this)
+                    {:service url
+                     :state "ok"
+                     :time (int (/ (System/currentTimeMillis) 1000))
+                     :tags ["grunf"]
+                     :description "query time"
+                     :metric (- (System/currentTimeMillis) start)
+                     :ttl (/ (* ttl 2) 1000)
+                     })
+        (catch IOException e))))
   (log-validate-error [this]
     (fn [{{start :start
            url :url
            ttl :interval} :opts}]
-      (send-event (.client this)
-                  {:service url
-                   :state "warning"
-                   :time (int (/ (System/currentTimeMillis) 1000))
-                   :tags ["grunf"]
-                   :description "validation error"
-                   :metric (- (System/currentTimeMillis) start)
-                   :ttl (/ (* ttl 2) 1000)
-                   })))
+      (try (send-event (.client this)
+                       {:service url
+                        :state "warning"
+                        :time (int (/ (System/currentTimeMillis) 1000))
+                        :tags ["grunf"]
+                        :description "validation error"
+                        :metric (- (System/currentTimeMillis) start)
+                        :ttl (/ (* ttl 2) 1000)
+                        })
+           (catch IOException e))))
   (log-redirect [this] (fn [_]))
   (log-client-error [this] (grunf.core/log-unknown-error this))
   (log-server-error [this] (grunf.core/log-unknown-error this))
@@ -40,14 +44,15 @@
            ttl :interval} :opts
            error :error
            status :status}]
-      (send-event (.client this)
-                  {:service url
-                   :state "error"
-                   :time (int (/ (System/currentTimeMillis) 1000))
-                   :tags ["grunf"]
-                   :description (str status ", error:" error)
-                   :metric (- (System/currentTimeMillis) start)
-                   :ttl (/ (* ttl 2) 1000)
-                   })))
+      (try (send-event (.client this)
+                       {:service url
+                        :state "error"
+                        :time (int (/ (System/currentTimeMillis) 1000))
+                        :tags ["grunf"]
+                        :description (str status ", error:" error)
+                        :metric (- (System/currentTimeMillis) start)
+                        :ttl (/ (* ttl 2) 1000)
+                        })
+           (catch IOException e))))
   )
 
