6c0d18eec699f460f63d97ccd04fe2a13cafe3e4
Aleksandar Bilanovic <miyazaki.mooo.com>
2014-01-06 18:52:52 +0000

    replaced private with dummy data

--- conf.example.clj
+++ conf.example.clj
@@ -1,6 +1,6 @@
 [{
   :riemann-service "fcl"
-  :url "http://198.54.96.98/search/TR110"
+  :url "http://192.168.0.1/search/TR110"
   :interval 10000
   :http-options {:timeout 2000
                  :user-agent "Grunf 0.4.02"}
@@ -8,7 +8,7 @@
   }
  {
   :riemann-service "search-soa"
-  :url "http://198.54.96.98:8090/search-soa/search/partFamily/0/1?search=max232"
+  :url "http://192.168.0.1:8090/search-soa/search/pF/0/1?search=max232"
   :method :put
   :headers {"Content-Type" "application/json"
             "Accept" "application/json"}
@@ -19,19 +19,19 @@
   :riemann-tags ["1w" "search-soa"]
   }
  {
-  :riemann-service "sep"
-  :url "http://198.54.96.98:9080/solr/sep/search?search=analog&linkFilter=SEP&partnerName=123"
+  :riemann-service "esp"
+  :url "http://192.168.0.1:9080/solr/esp/search?search=analog&linkFilter=ESP&partnerName=abc"
   :interval 10000
   :http-options {:timeout 2000
                  :user-agent "Grunf 0.4.02"}
-  :riemann-tags ["1w" "sep"]
+  :riemann-tags ["1w" "esp"]
   }
  {
-  :riemann-service "projects.hackaday.com"
-  :url "http://alpha.projects.hackaday.supplyframe.com"
+  :riemann-service "projects.hd.com"
+  :url "http://alpha.projects.hd.com"
   :interval 10000
   :http-options {:timeout 2000
                  :user-agent "Grunf 0.4.02"}
-  :riemann-tags ["1w" "projects.hackaday.com"]
+  :riemann-tags ["1w" "projects.hd.com"]
   }
 ]
